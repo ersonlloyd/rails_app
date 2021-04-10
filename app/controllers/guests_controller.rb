372b23cc4d1fdadf6f9c7f6d27f9ede03de55b50
Lloyd Francisco <lloyd@proudcloud.io>
2021-04-10 09:25:51 +0800

    Initial setup

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,13 @@
+class GuestsController < ApplicationController
+
+  def show
+  end
+
+  def create
+  end
+
+  private
+
+  def guest_reservation_params()
+  end
+end
---
+++ app/models/guest.rb
@@ -0,0 +1,7 @@
+class Guest < ApplicationRecord
+
+  field :first_name     type: String
+  field :last_name      type: String
+  field :phone          type: String
+  field :email          type: String
+end
