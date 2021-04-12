dba7a2b6002062b2424fe46b7c87919a86557abc
Lloyd Francisco <lloyd@proudcloud.io>
2021-04-12 11:06:53 +0800

    Adds routes

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,2 @@
 class Guest < ApplicationRecord
-
-  field :first_name     type: String
-  field :last_name      type: String
-  field :phone          type: String
-  field :email          type: String
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,19 +1,2 @@
 class Reservation < ApplicationRecord
-
-  field :start_date,                      type: DateTime
-  field :end_date,                        type: DateTime
-  field :expected_payout_amount,          type: String
-  field :listing_security_price_accurate  type: String
-  field :host_currency                    type: String
-  field :nights                           type: String
-  field :number_of_guests                 type: String
-  field :status_type                      type: String
-  field :total_paid_amount_accurate       type: String
-  field :guests                           type: String
-  field :adults                           type: String
-  field :infants                          type: String
-  field :currency                         type: String
-  field :payout_price                     type: String
-  field :security_price                   type: String
-  field :total_price                      type: String
 end
